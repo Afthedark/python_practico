28c782f64aeda4c40ce352a26ab124677ebe5808
Luis Gonzalo <afthedark@hotmail.com>
2024-07-05 17:34:02 -0400

    89

--- 03_titpos_numericos.py
+++ 03_titpos_numericos.py
@@ -1,4 +1,5 @@
 
+"""
 print("===================NUM ENTEROS=======================")
 
 n1 = int(input("Ingrese un numero entero: "))
@@ -9,7 +10,57 @@ print(f"Resultado: {n1 + n2}")
 
 print("===================DIV ENTEROS=======================")
 
-n1 = int(input("Ingrese un numero entero: "))
-n2 = int(input("Ingrese otro numero entero: "))
+n1 = int(input("Dividendo: "))
+n2 = int(input("Divisor: "))
 
 print(f"Resultado: {n1 // n2}")
+
+
+
+print("===================BASE EXPONTE ENTEROS=====================")
+
+
+base = int(input("Base: "))
+exponente = int(input("Exponente: "))
+
+print(f"Resultado {base ** exponente}")
+
+
+
+print("===================RESTA DECIMALES=====================")
+
+minuendo = float(input("Minuendo: "))
+sustraendo = float(input("Sustraendo: "))
+
+print(f"resultado {minuendo - sustraendo}")
+
+
+
+print("===================DIVISION REALES=====================")
+
+diviendo = float(input("Dividiendo: "))
+divisor = float(input("Divisor: "))
+
+print(f"Resultado {diviendo / divisor}")
+
+
+
+print("===================Multiplicacion REALES=====================")
+
+multiplicando = float(input("Multiplicando: "))
+multiplicador = float(input("Multiplicador: "))
+
+print(f"Resultado {multiplicando * multiplicador}")
+
+"""
+
+print("===================Multiplicacion REALES REDONDEANDO=====================")
+
+multiplicando = float(input("Multiplicando: "))
+multiplicador = float(input("Multiplicador: "))
+res = round(multiplicando * multiplicador, 1)
+
+print(f"Resultado {res}")
+
+
+#continuar 89
