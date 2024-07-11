e0776abc2f947d5c2a3e6406a784a5143ee28691
Luis Gonzalo <afthedark@hotmail.com>
2024-07-11 17:37:48 -0400

    continuar_120

---
+++ 07_porciones_cadena.py
@@ -0,0 +1,45 @@
+
+"""
+print("---------------------Porciones de cadena----------------------------")
+
+cadena = "F.C.Barcelona, Atletico de Madrid, Real Madrid"
+
+print("Primer equipo", (cadena[0:13]))
+print("Segundo equipo", (cadena[15:33]))
+print("Tercer equipo", (cadena[0:13]))
+
+print("Desde el principio de la cadena", cadena[:13])
+print("Desde el final de la cadena", cadena[15:])
+
+
+"""
+
+print("---------------------F string----------------------------")
+
+
+n1 = float(input("Primer numero: "))
+n2 = float(input("Segundo numero: "))
+
+
+suma = n1 + n2
+resta = n1 - n2
+div = n1 / n2
+multi = n1 * n2
+div_entera = n1 // n2
+mod = n1 % n2
+potencia = n1 ** n2
+
+
+print(f"La suma es: {suma}")
+print(f"La resta es: {resta}")
+print(f"La division es: {div}")
+print(f"La multiplicacion es: {multi}")
+print(f"La division entera es: {div_entera}")
+print(f"El modulo es: {mod}")
+print(f"La potencia es: {potencia}")
+
+
+
+
+
+
---
+++ 08_listas.py
@@ -0,0 +1,37 @@
+
+
+"""
+
+
+print("---------------------Listas----------------------------")
+
+
+lista = ["Python", "RA-MA", 2019, "Libro", 3]
+print(lista)
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
+print(lista[4])
+"""
+
+
+
+print("---------------------Concatenando Listas----------------------------")
+
+lista1 = ["Python", "RA-MA", 2019, "Libro", 3]
+lista2 = ["Javascript", "RA-MA", 2020, "Libro", 3]
+
+listaconcat = lista1 + lista2
+
+print("Numero elementos lista 1: ", len(lista1))
+print("Solo Lista 1", lista1)
+
+print("Numero elementos lista 2: ", len(lista2))
+print("Solo Lista 2", lista2)
+
+print("Lista 1 + lista 2 concatenada: ", listaconcat)
+print(listaconcat)
+
+
+#continuar 120
