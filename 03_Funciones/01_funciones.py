44f88e8df4def60098e7772d5a8be850f1711f19
Luis Gonzalo <afthedark@hotmail.com>
2024-07-24 17:36:38 -0400

    167 page

--- 03_Funciones/01_funciones.py
+++ 03_Funciones/01_funciones.py
@@ -64,7 +64,7 @@ multiplicador = int(input("Ingrese otro numero: "))
 res = Multiplicar(multiplicando, multiplicador)
 print(f"La multiplicacion es: {res}")
 
-"""
+
 
 
 
@@ -81,17 +81,38 @@ def SumarMultiplicar(*valores):
         resultadomultiplicacion = resultadomultiplicacion * item #multiplicar los valores
     return resultadosuma, resultadomultiplicacion
 
+
+
 ressuma, resmulti = SumarMultiplicar(3, 5, 7, 9, 11, 13, 15, 17, 19, 21)
 
 print(f"El resultado de la suma es: {ressuma}")
 print(f"El resultado de la multiplicacion es: {resmulti}")    
 
+"""
+
+
+def SumarMultiplicar(param1, param2):
+    return Sumar(param1, param2), Multiplicar(param1, param2)
+
+
+def Sumar(n1, n2):
+    return n1 + n2
+
+
+def Multiplicar(n1, n2):
+    return n1 * n2
+
+
+num1 = int(input("Introduce un numero 1: "))
+num2 = int(input("Introduce un numero 2: "))
+
+res_suma, res_multi = SumarMultiplicar(num1, num2)
+
+print(f"La suma es: {res_suma}")
+print(f"La multi es: {res_multi}")
+
+
+
+
 
 #continuar 167
-
-
-
-
-
-
-
